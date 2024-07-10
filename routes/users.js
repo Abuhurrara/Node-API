abb055b9f60b642da058eaf2ca706133e349d7bd
Abu Hurrara <abuhurrara1093@gmail.com>
2024-07-10 12:59:32 +0500

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const router = express.Router();
+
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 24
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 25
+    }
+]
+
+router.get('/', (req, res) => {
+    console.log(users);
+    res.send(users);
+})
+
+export default router;
